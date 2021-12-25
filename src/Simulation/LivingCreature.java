1648dd212929f7c1cd1718791dfe34e2120c7be9
Bennaceur Zerhouni <bennaceur.zerhouni@grenoble-inp.org>
2021-12-25 20:22:58 +0100

    Refonte

---
+++ src/Simulation/LivingCreature.java
@@ -0,0 +1,15 @@
+package Simulation;
+
+public abstract class LivingCreature {
+
+    private int lifePoints = 10;
+
+    public int getLifePoints() {
+        return lifePoints;
+    }
+
+    public void setLifePoints(int lifePoints) {
+        this.lifePoints = lifePoints;
+    }
+
+}
---
+++ tests/TestAquarium.java
@@ -0,0 +1,8 @@
+public class TestAquarium {
+
+    static final String[] names = {"Némo", "Néo", "Arthur", "Rocky", "Mickey", "Matthew", "Foggy", "Karen", "Carl", "Tom", "Ben", "Ferguson"};
+
+    public static void main(String[] args) {
+
+    }
+}
